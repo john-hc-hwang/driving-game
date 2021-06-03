690ebd1045ba89512f9b72610ae4481c1e69c14a
John Hwang <johnhchwang@gmail.com>
2021-06-03 15:24:21 -0700

    completed issue-5

--- main.js
+++ main.js
@@ -21,32 +21,56 @@ var direction = [
 ];
 
 var left = 0;
-// var right = 0;
-// var top = 0;
-// var bottom = 0;
-
+var topp = 0;
+var leftPx;
+var topPx;
 var spaceIndex = 0;
 
 function go() {
-  left += 16;
-  var leftPx = left + 'px';
-  $f1.style.left = leftPx;
+  if ($f1.className === 'f1 west') {
+    left -= 16;
+    leftPx = left + 'px';
+    $f1.style.left = leftPx;
+  }
+
+  if ($f1.className === 'f1 east') {
+    left += 16;
+    leftPx = left + 'px';
+    $f1.style.left = leftPx;
+  }
+
+  if ($f1.className === 'f1 north') {
+    topp -= 16;
+    topPx = topp + 'px';
+    $f1.style.top = topPx;
+  }
+
+  if ($f1.className === 'f1 south') {
+    topp += 16;
+    topPx = topp + 'px';
+    $f1.style.top = topPx;
+  }
 }
 
 var intervalId = setInterval(go, 16); // make intervalId global scope
 clearInterval(intervalId); // clearInterval asap to prevent it from moving when page is refreshed
 
 window.addEventListener('keydown', function (event) {
-  if (event.key === ' ') {
-    spaceIndex++;
-    if (spaceIndex % 2 === 1) intervalId = setInterval(go, 16);
-    if (spaceIndex % 2 === 0) clearInterval(intervalId);
-  }
-
   if (event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'ArrowDown' || event.key === 'ArrowUp') {
     $f1.className = 'f1';
     for (var dirs of direction) {
       if (dirs.key === event.key) $f1.classList.add(dirs.dir);
+    }
+  }
+
+  if (event.key === ' ') {
+    spaceIndex++;
+    if (spaceIndex % 2 === 1) {
+      intervalId = setInterval(go, 16);
+    }
+
+    if (spaceIndex % 2 === 0) {
+      clearInterval(intervalId);
     }
   }
 });

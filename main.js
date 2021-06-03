1435a38fc7f1946bfaf9a5641cc404f37fbfd59b
John Hwang <johnhchwang@gmail.com>
2021-06-03 13:21:23 -0700

    completed issue-3

--- main.js
+++ main.js
@@ -20,9 +20,26 @@ var direction = [
   }
 ];
 
+var left = 0;
+// var right = 0;
+// var top = 0;
+// var bottom = 0;
+
+function go() {
+  left += 16;
+  var leftPx = left + "px";
+  $f1.style.left = leftPx;
+}
+
 window.addEventListener('keydown', function (event) {
-  $f1.className = 'f1';
-  for (var dirs of direction) {
-    if (dirs.key === event.key) $f1.classList.add(dirs.dir);
+  if (event.key === " ") {
+    var intervalId = setInterval(go, 16);
+  }
+
+  if (event.key === "ArrowLeft" || event.key === "ArrowRight" || event.key === "ArrowDown" || event.key === "ArrowUp") {
+    $f1.className = 'f1';
+    for (var dirs of direction) {
+      if (dirs.key === event.key) $f1.classList.add(dirs.dir);
+    }
   }
 });

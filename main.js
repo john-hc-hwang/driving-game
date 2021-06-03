fafc228eee4d8e9f2902970fe30f6842c6bef6bd
John Hwang <johnhchwang@gmail.com>
2021-06-03 12:45:56 -0700

    completed issue-2

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-// var $ = document.querySelector.bind(document);
-// var $f1 = $(".f1");
+var $ = document.querySelector.bind(document);
+var $f1 = $('.f1');
 
-// var direction = [
-//   {
-//     dir: "west",
-//     key: "ArrowLeft"
-//   },
-//   {
-//     dir: "south",
-//     key: "ArrowDown"
-//   },
-//   {
-//     dir: "north",
-//     key: "ArrowUp"
-//   },
-//   {
-//     dir: "east",
-//     key: "ArrowRight"
-//   }
-// ];
+var direction = [
+  {
+    dir: 'west',
+    key: 'ArrowLeft'
+  },
+  {
+    dir: 'south',
+    key: 'ArrowDown'
+  },
+  {
+    dir: 'north',
+    key: 'ArrowUp'
+  },
+  {
+    dir: 'east',
+    key: 'ArrowRight'
+  }
+];
 
-// window.addEventListener("keydown", function(event) {
-//   console.log(event.key);
-// });
+window.addEventListener('keydown', function (event) {
+  $f1.className = 'f1';
+  for (var dirs of direction) {
+    if (dirs.key === event.key) $f1.classList.add(dirs.dir);
+  }
+});
